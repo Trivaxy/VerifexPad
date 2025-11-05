ed2f635a1ff4dfda551f319d54c43a7d2737e973
Trivaxy <itsmeomar2003@yahoo.com>
2025-11-05 11:18:03 +0300

    listen on public interface

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`VerifexPad backend server running on port ${PORT}`);
 });
